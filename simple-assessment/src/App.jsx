bf589654ab3d5823c6ddd8b3e5927d35fadb0a7e
karth1kkk <karthikeyanbalamurugan4@gmail.com>
2025-03-21 09:52:37 +0800

    location-change

--- simple-assessment/src/App.jsx
+++ simple-assessment/src/App.jsx
@@ -35,7 +35,7 @@ const App = () => {
 
   useEffect(() => {
     // fetching the CSV file from the specified location(referred chatgpt as ive not used papaparse)
-    fetch("/data/Table_Input.csv")
+    fetch("/Table_Input.csv")
       .then((response) => response.text()) 
       .then((csvText) => {
         Papa.parse(csvText, {
